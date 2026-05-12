3f7f769aa7a1807cea5c700db30640d6a961ebd0
fan.jin <fan.jin@gareatech.com>
2026-05-12 17:34:09 +0800

    新增导航页

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'production' ? '/niuniu_home/' : '/',
+  server: {
+    open: '/nav.html',
+  },
 }))
